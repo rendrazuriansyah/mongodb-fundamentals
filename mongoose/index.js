ea3e7598055b3836fc0f119924d8e3366350cb6b
Rendra Zuriansyah <rendra.zuriansyah.dev@gmail.com>
2025-05-28 20:30:32 +0700

    feat(mongoose): add basic movie schema and model - create movieSchema to define movie data structure - implement Movie model for data interaction - add example data for testing and demonstration - save movie data to database

--- mongoose/index.js
+++ mongoose/index.js
@@ -8,3 +8,27 @@ mongoose
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
+
+// membuat schema
+const movieSchema = new mongoose.Schema({
+  title: String,
+  year: Number,
+  score: Number,
+  director: String,
+});
+
+// membuat model
+const Movie = mongoose.model('Movie', movieSchema);
+
+// menggunakan model untuk membuat stage data
+const movie = new Movie({
+  title: 'Parasite',
+  year: 2019,
+  score: 9.5,
+  director: 'Bong Joon Ho',
+});
+// menampilkan stage data di console
+console.log(movie);
+
+// menyimpan data ke database
+movie.save();
