23a6b5d04c824d295b1cf1bc642a7b2d19cbe77e
Rendra Zuriansyah <rendra.zuriansyah.dev@gmail.com>
2025-06-02 09:10:24 +0700

    coba bikin schema dengan mcm" jenis data

--- mongoose/product.js
+++ mongoose/product.js
@@ -14,9 +14,46 @@ const productSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  brand: {
+    type: String,
+    required: true,
+  },
   price: {
     type: Number,
     required: true,
+    min: 0,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: [String],
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 150,
+  },
+  condition: {
+    type: String,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  availability: {
+    online: {
+      type: Boolean,
+      required: true,
+    },
+    offline: {
+      type: Boolean,
+      required: true,
+    },
   },
 });
 
