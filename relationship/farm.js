fa502385d1ef148e00fc1b35cb60011710f7b948
Rendra Zuriansyah <rendra.zuriansyah.dev@gmail.com>
2025-06-05 12:28:23 +0700

    nyoba nampilin data turunan dari relasi one to many pke populate, (param string maupun object serta array)

--- relationship/farm.js
+++ relationship/farm.js
@@ -34,73 +34,91 @@ const farmSchema = new mongoose.Schema({
 const Product = mongoose.model('Product', productSchema);
 const Farm = mongoose.model('Farm', farmSchema);
 
-Product.insertMany([
-  {
-    name: 'apple',
-    price: 5000,
-    season: 'fall',
-  },
-  {
-    name: 'banana',
-    price: 10000,
-    season: 'summer',
-  },
-  {
-    name: 'orange',
-    price: 15000,
-    season: 'winter',
-  },
-  {
-    name: 'pineapple',
-    price: 20000,
-    season: 'spring',
-  },
-  {
-    name: 'grape',
-    price: 25000,
-    season: 'summer',
-  },
-])
-  .then((result) => {
-    console.log('Products inserted:', result);
-  })
-  .catch((error) => {
-    console.error('Error inserting products:', error);
-  });
+// Product.insertMany([
+//   {
+//     name: 'apple',
+//     price: 5000,
+//     season: 'fall',
+//   },
+//   {
+//     name: 'banana',
+//     price: 10000,
+//     season: 'summer',
+//   },
+//   {
+//     name: 'orange',
+//     price: 15000,
+//     season: 'winter',
+//   },
+//   {
+//     name: 'pineapple',
+//     price: 20000,
+//     season: 'spring',
+//   },
+//   {
+//     name: 'grape',
+//     price: 25000,
+//     season: 'summer',
+//   },
+// ])
+//   .then((result) => {
+//     console.log('Products inserted:', result);
+//   })
+//   .catch((error) => {
+//     console.error('Error inserting products:', error);
+//   });
 
 // one to one
-const makeFarm = async () => {
-  const farm = new Farm({
-    name: 'Rendra Farm',
-    city: 'Anytown',
-  });
+// const makeFarm = async () => {
+//   const farm = new Farm({
+//     name: 'Rendra Farm',
+//     city: 'Anytown',
+//   });
 
-  const grape = await Product.findOne({
-    name: 'grape',
-  });
+//   const grape = await Product.findOne({
+//     name: 'grape',
+//   });
 
-  farm.products.push(grape);
+//   farm.products.push(grape);
 
-  const res = await farm.save();
+//   const res = await farm.save();
 
-  console.log(res);
-};
-makeFarm();
+//   console.log(res);
+// };
+// makeFarm();
 
 // one to many (krn sebelumya uda ditambahkan product)
-const addProduct = async () => {
-  const farm = await Farm.findOne({
-    name: 'Rendra Farm',
+// const addProduct = async () => {
+//   const farm = await Farm.findOne({
+//     name: 'Rendra Farm',
+//   });
+
+//   const apple = await Product.findOne({
+//     name: 'apple',
+//   });
+
+//   farm.products.push(apple);
+
+//   const res = await farm.save();
+
+//   console.log(res);
+// };
+// addProduct();
+
+// cara nampilin data turunan dari relasi one to many pke populate()
+// parameter pertama=> field relasi
+// parameter kedua=> field mana aja yg ditampil
+// bisa eksplisit pke object params
+// bisa exclude field dengan '-field'
+// pke array untuk ngebungkus si objek klo mau populate lebih dri satu
+// *bisa nested populate
+Farm.findOne({
+  name: 'Rendra Farm',
+})
+  .populate({ path: 'products', select: 'name' })
+  .then((result) => {
+    console.log(result);
+    // for (const product of result.products) {
+    //   console.log(product);
+    // }
   });
-
-  const apple = await Product.findOne({
-    name: 'apple',
-  });
-
-  farm.products.push(apple);
-
-  const res = await farm.save();
-
-  console.log(res);
-};
-addProduct();
